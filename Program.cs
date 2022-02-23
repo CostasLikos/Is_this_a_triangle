2139818e54f4449a8b4d3da22876f118e3ff1662
Costas Likos <costaslikos@gmail.com>
2022-02-23 14:33:29 +0200

    x

--- Program.cs
+++ Program.cs
@@ -4,8 +4,8 @@ public class Triangle
 {
     public static bool IsTriangle(int a, int b, int c)
     {
-        int[] arr = new int[] { a, b, c };
-        Array.Sort(arr);
-        return ((arr[0] + arr[1]) > arr[2]);
+        int[] x = new int[] { a, b, c };
+        Array.Sort(x);
+        return ((x[0] + x[1]) > x[2]);
     }
 }
